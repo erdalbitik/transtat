dfd8a30d81c482c0e73abe0091d1d4b9df2b92d7
erdal.bitik <erdalbitik@gmail.com>
2017-06-19 04:15:43 +0300

    test refactoring

--- src/test/java/com/ebitik/transtat/ApplicationTests.java
+++ src/test/java/com/ebitik/transtat/ApplicationTests.java
@@ -2,6 +2,9 @@ package com.ebitik.transtat;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -22,21 +25,49 @@ import com.ebitik.transtat.service.TranStatService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-	
+
 	private static Logger logger = Logger.getLogger(ApplicationTests.class);
-	
+
 	@Autowired TranStatService service;
-	
+
 	@Test
-	public void test1() throws InterruptedException {
-		addNRandomTransaction(5000);
-		Statistic statistic = service.getStatistic();
-		logger.debug("Statistic: "+statistic);
+	public void multiThreadingTest() throws InterruptedException {
+		addNRandomTransactionMultiThreaded(10);
+
+		//IntStream.rangeClosed(1, 13).forEach(i -> {
+			Statistic statistic = service.getStatistic();
+			logger.info("Statistic: "+statistic);
+			/*try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+			}*/
+		//});
+
 	}
-	
-	
+
+	private void addNRandomTransactionMultiThreaded(int n) {
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+
+		IntStream.range(0, n)
+		.forEach(i -> executor.submit(() -> {
+			Transaction transaction = new Transaction();
+			transaction.setTimestamp(TestUtil.getRandomMinuteTime());
+			BigDecimal amount = BigDecimal.valueOf(TestUtil.getRandomAmount());
+			amount = amount.setScale(2, RoundingMode.HALF_UP);
+			transaction.setAmount(amount);
+			try {
+				service.addTransaction(transaction);
+			} catch (LateTransactionException e) {
+				logger.debug("Late Transaction Occured for "+transaction);
+			} }
+				));
+
+		executor.shutdown();
+	}
+
+
 	private void addNRandomTransaction(int n) {
-		for (int i = 0; i < n; i++) {
+		IntStream.rangeClosed(1, n).parallel().forEach(i -> {
 			Transaction transaction = new Transaction();
 			transaction.setTimestamp(TestUtil.getRandomMinuteTime());
 			BigDecimal amount = BigDecimal.valueOf(TestUtil.getRandomAmount());
@@ -47,7 +78,7 @@ public class ApplicationTests {
 			} catch (LateTransactionException e) {
 				logger.debug("Late Transaction Occured for "+transaction);
 			}
-		}
+		});
 	}
 
 }
